39951f8833dc5efa7f195c066b4e5e15282155e8
xupeixuan <henryviolet@sina.com>
2018-08-31 21:16:42 +0800

    [Changed] Add comment for elements in annotation.

--- src/main/java/com/genghis/carpo/test/spring/mybatis/context/MybatisTestProperty.java
+++ src/main/java/com/genghis/carpo/test/spring/mybatis/context/MybatisTestProperty.java
@@ -11,12 +11,29 @@ import java.lang.annotation.*;
 @Inherited
 public @interface MybatisTestProperty {
 
+    /**
+     * Location of the MyBatis {@code SqlSessionFactory} config file. A typical value is
+     * "WEB-INF/mybatis-configuration.xml".
+     */
     String configLocation() default "";
 
+    /**
+     * Locations of MyBatis mapper files that are going to be merged into the configuration at runtime.
+     *
+     * This is an alternative to specifying "&lt;sqlmapper&gt;" entries in an MyBatis config file.
+     * This property being based on Spring's resource abstraction also allows for specifying
+     * resource patterns here: e.g. "classpath*:sqlmap/*-mapper.xml".
+     */
     String[] mapperLocations() default {};
 
+    /**
+     * The mapper interfaces of the MyBatis mapper
+     */
     Class[] classes() default {};
 
+    /**
+     * List of type aliases to register. They can be annotated with {@code Alias}
+     */
     Class[] typeAliases() default {};
 
 }
